20e85a82eb61a14b63a1ae545b963ee718d5879a
Ericsen Tsai <bommer95175@gmail.com>
2025-07-02 22:27:46 +0800

    chore: tweak config files

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -19,7 +19,14 @@ function App() {
       <h1>React Browse Autoplay</h1>
       <div style={{ height: '100vh' }}></div>
       <AutoplayAnchor type="top" />
-      <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         {isEnabledAutoplay ? 'Autoplay Enabled' : 'Autoplay Disabled'}
       </div>
       <AutoplayAnchor type="bottom" />
--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -4,4 +4,19 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-browse-autoplay/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+    force: true,
+  },
+  server: {
+    fs: {
+      allow: ['../..'],
+    },
+  },
 });
